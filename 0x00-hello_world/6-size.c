19307207c7bf0f47d2eec6a9d694d0f944ed6bca
SharonWendoh <swendoh.sharon@gmail.com>
2023-07-16 07:24:46 -0700

    Betty coding style and betty documentation style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,9 @@
 /**
  * main - prints out the sizes of the avrious data types in C
  *
- * Return : Always 0
+ * Return : Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
