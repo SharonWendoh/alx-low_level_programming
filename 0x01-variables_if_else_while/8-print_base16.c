4c143de491b484218ad40a221c19a016824def33
SharonWendoh <swendoh.sharon@gmail.com>
2023-07-17 10:28:58 -0700

    Added 8 and 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	int x = 0;
+	char hexDigits[] = "0123456789abcdef";
+
+	while (x <= 16)
+	{
+		putchar(hexDigits[x]);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
