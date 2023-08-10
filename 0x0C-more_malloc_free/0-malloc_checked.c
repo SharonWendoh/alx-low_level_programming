f2c03c7744e6212b1556c01dc636cd9721021920
SharonWendoh <swendoh.sharon@gmail.com>
2023-08-10 10:13:24 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - creates an array of chars
+ *
+ * @b: size
+ *
+ * Return: pointer to memory or 98 otherwise.
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *x;
+
+	x = malloc(b);
+
+	if (x == NULL)
+	{
+		exit (98);
+	}
+	return (x);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This folder contains more exercies on malloc
