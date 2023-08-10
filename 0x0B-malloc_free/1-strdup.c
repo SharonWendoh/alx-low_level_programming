9866bf4d4ce6be98b851686949fba56f374bcfde
SharonWendoh <swendoh.sharon@gmail.com>
2023-08-09 18:28:09 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - creates an array of chars
+ *
+ * @str: string to be copied
+ *
+ * Return: pointer to string or null otherwise.
+ */
+char *_strdup(char *str)
+{
+	char *t;
+	unsigned int i;
+	size_t length = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	t = malloc(sizeof(str) * length);
+
+	if (t == NULL)
+		return (NULL);
+
+	for (i = 0; i < length; i++)
+	{
+		t[i] = str[i];
+	}
+	return (t);
+}
