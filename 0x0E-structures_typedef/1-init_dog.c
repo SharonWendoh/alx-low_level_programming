65743bd35fa6c149600c5e255106274e92999300
Sharon Wendoh <75203077+SharonWendoh@users.noreply.github.com>
2023-08-15 05:28:18 +0300

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,11 +2,11 @@
 #include "dog.h"
 /**
  * init_dog - Initializes a dog variable
- *
  * @d: The struct
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ *
  * Return: no return
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
