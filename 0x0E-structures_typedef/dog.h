01890c4c8501ba32f0a708a6f324ab62c0ff5c84
SharonWendoh <swendoh.sharon@gmail.com>
2023-08-14 18:45:10 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog
+ *
+ * @name: First Member
+ * @age: Second member
+ * @owner: Third member
+ * 
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
