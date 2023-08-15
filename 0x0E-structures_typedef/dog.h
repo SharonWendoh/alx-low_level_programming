569d3c92ffbc4f7a68c25075e55bb0eac5cca530
SharonWendoh <swendoh.sharon@gmail.com>
2023-08-14 18:57:14 -0700

    correct task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog
+ * struct dog - Represents dog information
  *
  * @name: First Member
  * @age: Second member
  * @owner: Third member
- * 
+ *
  * Description: Longer description
  */
 struct dog
