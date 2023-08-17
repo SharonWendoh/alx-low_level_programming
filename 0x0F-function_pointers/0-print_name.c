f0e22a7108201f072addaa8e47c3a8d3c5e5b2f4
SharonWendoh <swendoh.sharon@gmail.com>
2023-08-16 18:43:16 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - prints a name
+ *
+ * @name: name of person
+ * @f: callback function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This folder contains Tasks related to function pointers
